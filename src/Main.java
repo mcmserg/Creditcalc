f1f77cf7cae44e13dcdfe563a65923c29a36160d
Sergey <mcmserg@mail.ru>
2026-05-22 15:14:07 +0300

    added autotests

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,16 @@ public class Main {
         double bet = 9.99;
 
         double total = service.credit(summa, term, bet);
-        System.out.println(total);
+        System.out.println();
+        System.out.println("Сумма 1_000_000  месяц  12");
+        System.out.println("Результат " + total);
 
+        System.out.println();
+        System.out.println("Сумма  1_000_000  месяц 24");
+        System.out.println("Результат " + service.credit(summa = 1_000_000, term = 24, bet = 9.99));
+
+        System.out.println();
+        System.out.println("Сумма  1_000_000  месяц 36");
+        System.out.println("Результат " + service.credit(summa = 1_000_000, term = 36, bet = 9.99));
     }
 }
